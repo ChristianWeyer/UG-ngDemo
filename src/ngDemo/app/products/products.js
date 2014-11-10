1e051045680c256a09ba1a25b83c5c57c696c800
Christian Weyer <christian.weyer@thinktecture.com>
2014-11-10 22:28:00 +0100

    Starting point for simple ng demo

--- src/ngDemo/app/products/products.js
+++ src/ngDemo/app/products/products.js
@@ -1,17 +1,8 @@
 (function() {
     "use strict";
 
-    function ProductsListController($scope, productsService, $location) {
+    function ProductsListController() {
 
-        productsService.listProducts().then(function (result) {
-            $scope.products = result.data;
-        }, function (error) {
-            alert(JSON.stringify(error));
-        });
-
-        $scope.loadDetails = function (product) {
-            $location.path("/product/" + product.Id);
-        };
     };
 
     app.module.controller("productsListController", ProductsListController);
