0b216da93d0f47c7d653affcd203e98cfe12f720
Christian Weyer <christian.weyer@thinktecture.com>
2014-11-10 20:10:05 +0100

    Build script update

--- deploy/build.sh
+++ deploy/build.sh
@@ -2,15 +2,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${DIR} || exit
 
-
 SRC=`pwd`
 SRC=${SRC}/../src/ngDemo
-TARGET=/projects/myApp_build
-mkdir $TARGET 2>/dev/null
-cd $TARGET
 rm -rf out
 mkdir out
 cd out
+
 cordova create myApp com.thinktecture.myApp TTmyApp --copy-from ${SRC}
 
 cd myApp/www
@@ -18,16 +15,19 @@ rm -rf bin
 rm -rf obj
 rm index.cshtml
 rm cordova.js
+rm *.csproj
+rm *.config
+rm -rf Properties
+rm -rf Server
+
 curl http://windows8vm.local/ngdemo/ > index.html
 
-# make src/hrefs relative by removing leading "/"
-# from <script src="/apps/foo.js"> to <script src="apps/foo.js"> 
-sed -i -e 's/src=\"\//src=\"/g' index.html
-sed -i -e 's/href=\"\//href=\"/g' index.html
+sed -i -e 's/src=\"\/ngdemo\//src=\"/g' index.html
+sed -i -e 's/href=\"\/ngdemo\//href=\"/g' index.html
 
 cd ..
 cordova platform add ios
 cordova platform add android
+cordova platform add windows
 
-#cordova plugin add org.apache.cordova.device
-#cordova plugin add org.apache.cordova.console
+cordova plugin add org.apache.cordova.statusbar
